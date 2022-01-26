57fa540c413700ed9fb1a95e3b801e9f5e2f81c4
imane-karmouch <80288850+imane-karmouch@users.noreply.github.com>
2022-01-26 09:22:30 +0100

    Update Mc.py

--- Mc.py
+++ Mc.py
@@ -1,44 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 import statistics as sts
-#from scipy import linalg
+
 N=15
-x=np.linspace(0,5,N)
+x =np.array((np.linspace(0,5,N))).T
 y=(x-2)**2+np.random.randn(15)
 
 k=5
 A = []
-for i in range(N):
-    b=[]
-    for j in range(k+1):
-        b.insert(0,x[i]**j)
-    A.append(b)
-A=np.array(A)
+for i in range(k,-1,-1):
+  A.append((x**(i)))
+A = (np.array(A)).transpose
 
-what=np.array(np.linalg.lstsq(A, y, rcond = None)[0])
-
-
+what=(np.linalg.lstsq(A, y, rcond = None)[0])
 # evaluate the polynomial on evenly spaced points.
-# NOTE: you can also use the command yy = polyval(what,xx);
-
-xx = np.linspace(0,5,200)
-AA = [];
+xx = np.array(np.linspace(0,5,200)).T
+AA = []
 for i in range(k+1):
-    AA.append(xx**(k-i-1))
-
+    AA.append(xx**(k-i))
 AA=(np.array(AA)).transpose()
-
 yy = np.dot(AA,what)
 
 # plot the data points and the best fit
-plt.scatter(x,y)
-plt.plot(xx,yy, 'b-')
-plt.axis([0, 6, 0, 20])
-plt.title('best fit using a polynomial of degree',k)
+
+plt.scatter(x, y)
+plt.plot(xx , yy , 'b')
+plt.title("best fit using a polynomial of degree "+str(k)) 
 plt.show()
-
-
-#err = np.linalg.norm(y - np.dot(A,what))
+err1 = np.linalg.norm(y - np.dot(A,what))
 
 #NO CROSS-VALIDATAION
 kmax = 5
@@ -60,9 +49,7 @@ for k in range(1,kmax+1):
     what = np.array(np.linalg.lstsq(Amat, y, rcond = None)[0]) 
     # compute the error and divide by the number of points
     err[k-1]= (np.linalg.norm(y - np.dot(Amat,what)))/N #for each k
-
-k=5
-kmax=5
+    
 #USIGN CROSS-VALIDATION
 
 T = 12            # number of training points to use
@@ -104,34 +91,15 @@ for i in range(5):
 
 X = [1,2,3,4,5]
 Y1 = [avg_err_cv[i] for i in range(5)]
-Z = [errcv[i] for i in range(5)]
+y2= [err[i] for i in range(5)]
   
 X_axis = np.arange(len(X))
   
-plt.bar(X_axis - 0.2, Y1, 0.4, label = 'average test error')
-plt.bar(X_axis + 0.2, Z, 0.4, label = 'err')
+plt.bar(X_axis - 0.2, Y1, 0.4, label = 'using cross_validation')
+plt.bar(X_axis + 0.2, y2, 0.4, label = 'performance on training data')
   
 plt.xticks(X_axis, X)
 plt.xlabel("polynomial degree")
 plt.ylabel("average test error")
 plt.legend()
 plt.show()
-
-'''A = np.empty((15,6))
-for i in range(k,-1,-1):
-    X=np.empty((15,1))
-    X=x**i
-    A.concatenate(X)
-'''
-'''
-
-import 
-
-
-figure(2); clf
-bar([err avg_err_cv])
-xlabel('polynomial degree')
-ylabel('average test error')
-legend('performance on training data','using cross-validation',...
-    'Location','north')
-'''
